da9b5e4d4240d722980b3928f6f816d50267e1de
rohit0147 <rohitchoudhary0147@gmail.com>
2018-08-30 18:54:15 +0530

    final_submission

---
+++ lab6_160050030_160050059/src/Config.java
@@ -0,0 +1,9 @@
+
+/**
+ * Edit to provide your database credentials
+ */
+public class Config {
+	public static final String url = "jdbc:postgresql://localhost:5590/whatasap";
+	public static final String user = "sharvik";
+	public static final String password = "";
+}
